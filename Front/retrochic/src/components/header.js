620be250c3a4c1e529bd5b569f225bf138114e66
Jules OUTIN <jules.outin@ynov.com>
2024-05-08 12:16:17 +0200

    Mise en place de la navbar

---
+++ Front/retrochic/src/components/header.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import './header.css';
+import shop from '../assets/shop.png';
+import login from '../assets/login.png';
+
+function Header() {
+    const [search, setSearch] = useState('');
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        console.log(search);
+    }
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSearchSubmit(e);
+        }
+    }
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/catalogue">Catalogue</a>
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/75" alt="logo" />
+          </li> 
+          <li>
+            <a href="/about">About</a>
+          </li>
+            <li>
+                <input type="text" placeholder="Rechercher..." value={search} onChange={handleSearch} onKeyPress={handleKeyPress} />
+            </li>
+            <li>
+                <a href="/shop">
+                    <img src={shop} alt="shop" />
+                </a>
+            </li>
+            <li>
+                <a href="/login">
+                    <img src={login} alt="login" />
+                </a>
+            </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
