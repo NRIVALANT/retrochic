5feace6291c460155c8892a26f13db15febf7b23
Jules OUTIN <jules.outin@ynov.com>
2024-05-08 15:50:44 +0200

    Ajout page login

---
+++ Front/retrochic/src/pages/login.js
@@ -0,0 +1,13 @@
+
+
+function Login() {
+    return (
+        <div>
+            <div className="login-page">
+                <h1>Login</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
