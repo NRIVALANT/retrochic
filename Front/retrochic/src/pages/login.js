1e38a18fbf3687cf0e525900e3bbc414f1e11e9b
Jules OUTIN <jules.outin@ynov.com>
2024-05-08 16:09:40 +0200

    Mise en place des routes + début page login

--- Front/retrochic/src/pages/login.js
+++ Front/retrochic/src/pages/login.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+import '../assets/style/login.css';
 
 function Login() {
     return (
         <div>
             <div className="login-page">
                 <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit">Login</button>
+                </form>
+                <p>
+                    Don't have an account? <a href="/register">Register</a>
+                </p>
             </div>
         </div>
     )
