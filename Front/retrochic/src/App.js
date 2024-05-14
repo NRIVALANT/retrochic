ecc831d59b8e3ca5ec45d3d6477f7f865d5e3e31
Jules OUTIN <jules.outin@ynov.com>
2024-05-14 12:24:56 +0200

    Page du panier

--- Front/retrochic/src/App.js
+++ Front/retrochic/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Catalogue from './pages/catalogue';
 import About from './pages/about';
-// import Shop from './pages/shop';
+import Shop from './pages/shop';
 import Login from './pages/login';
 import Register from './pages/register';
 // import Header from './components/header';
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/catalogue" component={Catalogue} />
         <Route path="/about" component={About} />
-        {/* <Route path="/shop" component={Shop} /> */}
+        <Route path="/shop" element={<Shop />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
