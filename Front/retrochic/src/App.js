a741de6afa566264a4402a87de340c380c9be2f3
Jules OUTIN <jules.outin@ynov.com>
2024-05-14 15:48:19 +0200

    Page popup

--- Front/retrochic/src/App.js
+++ Front/retrochic/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home';
 import Catalogue from './pages/catalogue';
 import About from './pages/about';
 // import Shop from './pages/shop';
+import Popup from './pages/popup';
 import Login from './pages/login';
 import Register from './pages/register';
 // import Header from './components/header';
@@ -16,6 +17,7 @@ function App() {
         <Route path="/catalogue" component={Catalogue} />
         <Route path="/about" component={About} />
         {/* <Route path="/shop" component={Shop} /> */}
+        <Route path="/popup" element={<Popup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
